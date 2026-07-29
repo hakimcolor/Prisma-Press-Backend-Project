c84305e291187d4c20bb2b3dbe81ebc381959890
hakimcolor <hakimcolor777@gmail.com>
2026-07-29 12:26:31 +0600

    defiend the .env in config

---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+import dotenv from 'dotenv';
+import path from 'node:path';
+dotenv.config({ path: path.join(process.cwd(), '.env') });
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,19 @@
 import { log } from 'node:console';
 import app from './app';
 import { prisma } from './lib/prisma';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 async function main() {
   try {
     await prisma.$connect();
-    console.log("connected to the database successfully");
-    
+    console.log('connected to the database successfully');
+
     app.listen(PORT, () => {
-    console.log(`server is runing port on ${PORT}`)  
+      console.log(`server is runing port on ${PORT}`);
     });
   } catch (error) {
-    
     console.error('error ', error);
     await prisma.$disconnect();
     process.exit(1);
   }
 }
-main()
+main();
