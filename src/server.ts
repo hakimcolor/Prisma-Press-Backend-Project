c96a890c4c346cd092334272ef6c7d909c959390
hakimcolor <hakimcolor777@gmail.com>
2026-07-29 12:11:54 +0600

    connect

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import { log } from 'node:console';
 import app from './app';
+import { prisma } from './lib/prisma';
 const PORT = process.env.PORT || 5000;
 async function main() {
   try {
+    await prisma.$connect();
+    console.log("connected to the database successfully");
+    
     app.listen(PORT, () => {
-      `server is runing port on ${PORT}`;
+    console.log(`server is runing port on ${PORT}`)  
     });
   } catch (error) {
+    
     console.error('error ', error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
