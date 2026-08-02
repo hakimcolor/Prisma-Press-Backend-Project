072c2fcf12875bc60b0a53f7678a271a08eb3737
hakimcolor <hakimcolor777@gmail.com>
2026-08-02 23:18:34 +0600

    index ts fov config

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,13 +6,13 @@ export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   api_url: process.env.API_URL,
-  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+//   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
 
-  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+//   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 
-  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+//   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
 
-  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES,
+//   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES,
 
-  jwt_refresh_expires_in: process.env.JWT_REFESH_EXPIRES,
-};
+//   jwt_refresh_expires_in: process.env.JWT_REFESH_EXPIRES,
+// };
