4ed90c2ef36ce619bdf5c814024ffd0d94aeb139
hakimcolor <hakimcolor777@gmail.com>
2026-08-02 23:19:03 +0600

    bcrypt salt rounds

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   api_url: process.env.API_URL,
-//   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
 
 //   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 
