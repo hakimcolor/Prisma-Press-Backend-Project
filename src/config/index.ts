dd67c3039bf080a2315676e82743e51d9e5ff095
hakimcolor <hakimcolor777@gmail.com>
2026-08-02 23:19:54 +0600

    jwt refresh secet

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,7 @@ export default {
 
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
 
-//   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
 
 //   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES,
 
