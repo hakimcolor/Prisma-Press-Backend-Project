b7f71f96f8259ada20eedc2459b47daff31fa76e
hakimcolor <hakimcolor777@gmail.com>
2026-08-09 22:58:00 +0600

    ready all confige file for .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,17 @@
-
 import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 export default {
-
   port: process.env.PORT || 5000,
-  
-}
+  database_url: process.env.DATABASE_URL || '',
+  app_url: process.env.APP_URL || '',
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS || 10,
+jwt_access_secret: process.env.JWT_ACCESS_SECRET || '',
+jwt_refresh_secret: process.env.JWT_REFRESH_SECRET || '',
+jwt_access_expiration: process.env.JWT_ACCESS_EXPIRATION || '15m',
+jwt_refresh_expiration: process.env.JWT_REFRESH_EXPIRATION || '7d',
+auth_api_key: process.env.AUTH_API_KEY || '',
+
+};
