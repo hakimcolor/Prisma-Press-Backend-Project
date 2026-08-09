b9a67cebd47a8dbbf761db3d7a03b68331f85f5f
hakimcolor <hakimcolor777@gmail.com>
2026-08-09 23:06:26 +0600

    fix all issues in .env

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,6 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('yes server is running on port 5000');
+  res.send('yes server is running on port 3000');
 });
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
+import config from './config';
 
-const PORT = process.env.PORT ;
+const PORT = config.port;
 
 async function main() {
   try {
