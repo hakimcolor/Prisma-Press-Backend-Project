e3008be560e6db715794baae2abbcb762f665ccc
hakimcolor <hakimcolor777@gmail.com>
2026-08-15 21:57:09 +0600

    fix all

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
-
+app.get('/', (req: Request, res: Response) => {
+  res.send('your server is runing good and well ');
+});
 export default app;
