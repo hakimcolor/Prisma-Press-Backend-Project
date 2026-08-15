3f262fea253b93f2e3c465791ae4ff55d0ad8d45
hakimcolor <hakimcolor777@gmail.com>
2026-08-15 21:52:28 +0600

    defind port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,12 @@ import app from './app';
 const PORT = process.env.PORT || 5000;
 async function main() {
   try {
-    app.listen();
-  } catch (error) {}
+    app.listen(PORT, () => {
+      console.log(`server is runnning on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log('error starting the server', error);
+    process.exit(1);
+  }
 }
 main();
