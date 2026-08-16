3c940c83cd8b226e14b9ae90ec2c4617f95b39cd
hakimcolor <hakimcolor777@gmail.com>
2026-08-16 11:31:49 +0600

    install app and config all and  defiend medilware

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import app from './app';
-const PORT = process.env.PORT || 5000;
+import config from './config';
+import { prisma } from './lib/prisma';
+const PORT = config.port;
 async function main() {
   try {
+    await prisma.$connect();
+    console.log('prisma conneceted.');
     app.listen(PORT, () => {
       console.log(`server is runnning on port ${PORT}`);
     });
   } catch (error) {
     console.log('error starting the server', error);
+    await prisma.$disconnect();
     process.exit(1);
   }
 }
