f8d2677d0848bc0312e7cf58fea2ea719c85cb7c
hakimcolor <hakimcolor777@gmail.com>
2026-08-17 21:01:14 +0600

    add jwt

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,9 +7,9 @@ export default {
   port: process.env.PORT,
   database: process.env.DATABASE_URL,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-  jwt_access_token_secret: process.env.JWT_ACCESS_TOKEN_SECRET,
-  jwt_access_token_expiration: process.env.JWT_ACCESS_TOKEN_EXPIRATION,
-  jwt_refresh_token_secret: process.env.JWT_REFRESH_TOKEN_SECRET,
-  jwt_refresh_token_expiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION,
-  app_url:process.env.APP_URL
+  jwt_access_token_secret: process.env.JWT_ACCESS_TOKEN_SECRET!,
+  jwt_access_token_expiration: process.env.JWT_ACCESS_TOKEN_EXPIRATION!,
+  jwt_refresh_token_secret: process.env.JWT_REFRESH_TOKEN_SECRET!,
+  jwt_refresh_token_expiration: process.env.JWT_REFRESH_TOKEN_EXPIRATION!,
+  app_url: process.env.APP_URL,
 };
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
+import config from '../../config';
 import { prisma } from '../../lib/prisma';
 import { ILoginuser } from './auth.interface';
 import bcrypt from 'bcrypt';
+import jwt, { SignOptions } from 'jsonwebtoken';
 const loginUser = async (paylod: ILoginuser) => {
   const { email, password } = paylod;
   const user = await prisma.user.findUniqueOrThrow({
@@ -10,8 +12,20 @@ const loginUser = async (paylod: ILoginuser) => {
   if (!isPasswordMatch) {
     throw new Error('password is incorrect');
   }
-  return user;
+  const jwtpayload = {
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+  const accessToken = jwt.sign(jwtpayload, config.jwt_access_token_secret, {
+    expiresIn: config.jwt_access_token_expiration,
+  } as SignOptions);
+  const refreshToken = jwt.sign(jwtpayload, config.jwt_refresh_token_secret, {
+    expiresIn: config.jwt_refresh_token_expiration,
+  } as SignOptions);
+  return { accessToken, refreshToken };
 };
+
 export const authService = {
   loginUser,
 };
