53e54a1881ba326247d9b74d4a398fa4cfed3824
hakimcolor <hakimcolor777@gmail.com>
2026-08-26 22:02:50 +0600

    fix: for jwt acces and expir create a dinime function for easy cod and reusable

---
+++ src/utils/jwt.ts
@@ -0,0 +1,9 @@
+import { JwtPayload, SignOptions } from "jsonwebtoken";
+
+;
+const createToken = (payload: JwtPayload, secrect: string, expireIn: SignOptions) => {
+  
+}
+export const jutUtils = {
+  createToken
+}
