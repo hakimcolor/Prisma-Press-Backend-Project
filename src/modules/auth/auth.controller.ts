949100f48beba28966d6a7b6093d3190158d6ef2
hakimcolor <hakimcolor777@gmail.com>
2026-08-27 22:47:52 +0600

    add: refresh cookies for browser

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,16 +3,35 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../../utils/catchAsync';
 import { authService } from './auth.service';
 import { sendResponse } from '../../utils/sendResponse';
+import { ref } from 'node:process';
 
 const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
-    const loginResult = await authService.loginUser(payload);
+    const {accessToken,refreshToken} = await authService.loginUser(payload);
+    
+    // access cookis 
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'none',
+      maxAge: 1000*60*60*24
+})
+// refresh cookies
+res.cookie('refreshToken', refreshToken, {
+  httpOnly: true,
+  secure: false,
+  sameSite: 'none',
+  maxAge: 1000 * 60 * 60 * 7,
+});
+
     sendResponse(res, {
       success: true,
       StatusCodes: httpsStatus.OK,
       Message: 'user logged in successfully',
-      data: loginResult,
+      data: {
+        accessToken, refreshToken
+      }
     });
   }
 );
