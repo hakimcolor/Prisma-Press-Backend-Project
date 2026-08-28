728abd7d26ef5c7db121b69cd5f481bcbcdffee9
hakimcolor <hakimcolor777@gmail.com>
2026-08-28 23:13:53 +0600

    fix:verifiedToken  function in jwt also createToken and VerifiedToken

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,23 @@
-import jwt,{ JwtPayload, SignOptions } from "jsonwebtoken";
+import jwt, { JwtPayload, SignOptions } from 'jsonwebtoken';
 
-const createToken = (payload: JwtPayload, secret: string, expiresIn: SignOptions) => {
-  const token = jwt.sign(payload, secret, { expiresIn }as SignOptions)
-return token
-}
+const createToken = (
+  payload: JwtPayload,
+  secret: string,
+  expiresIn: SignOptions
+) => {
+  const token = jwt.sign(payload, secret, { expiresIn } as SignOptions);
+  return token;
+};
+const verifiedToken = (token: string, secret: string) => {
+  try {
+    const verifiedToken = jwt.verify(token, secret);
+    return verifiedToken;
+  } catch (error) {
+    throw new Error('invalid token ');
+  }
+};
+
 export const jwtUtils = {
-  createToken
-}
+  createToken,
+  verifiedToken,
+};
