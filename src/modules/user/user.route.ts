453e26202a83809751b8849914ebf17f1b91b659
hakimcolor <hakimcolor777@gmail.com>
2026-08-29 23:27:41 +0600

    add request and res

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -3,7 +3,7 @@ import { userController } from './user.controller';
 
 const router = Router();
 router.post('/register', userController.createUser);
-router.get('/me', () => {
+router.get('/me', (req, res) => {
   
 } ,userController.getMyprofile)
 export const userRoute = router;
