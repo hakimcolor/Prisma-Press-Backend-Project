823ec234dcaca1424d70896e305a6ce3ed634418
hakimcolor <hakimcolor777@gmail.com>
2026-08-29 23:36:12 +0600

    add next functon for call next fuction

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,10 @@
-import { Request, Router, Response } from 'express';
+import { Request, Router, Response, NextFunction } from 'express';
 import { userController } from './user.controller';
 import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 router.post('/register', userController.createUser);
-router.get('/me', (req :Request, res:Response) => {
+router.get('/me', (req :Request, res:Response , next:NextFunction) => {
   res.status(200).json({
     success: true,
     statucCode: 200,
