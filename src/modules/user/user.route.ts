cfe301a81e697a2968f699e14bc54296b558c130
hakimcolor <hakimcolor777@gmail.com>
2026-08-29 23:28:27 +0600

    add type for req and res

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Request, Router, Response } from 'express';
 import { userController } from './user.controller';
 
 const router = Router();
 router.post('/register', userController.createUser);
-router.get('/me', (req, res) => {
+router.get('/me', (req :Request, res:Response) => {
   
 } ,userController.getMyprofile)
 export const userRoute = router;
