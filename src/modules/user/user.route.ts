a857f6ea50f87394b2b7cc2af8a14085d252f89b
hakimcolor <hakimcolor777@gmail.com>
2026-08-29 23:34:29 +0600

    add midleware

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,9 +1,14 @@
 import { Request, Router, Response } from 'express';
 import { userController } from './user.controller';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
 router.post('/register', userController.createUser);
 router.get('/me', (req :Request, res:Response) => {
-  
+  res.status(200).json({
+    success: true,
+    statucCode: 200,
+    message:'your profile retrieved successfully '
+  })
 } ,userController.getMyprofile)
 export const userRoute = router;
