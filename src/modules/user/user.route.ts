93a876d99d6175bb40e5601d9cf8ee9424404f3e
hakimcolor <hakimcolor777@gmail.com>
2026-08-29 23:43:42 +0600

    fix and add middleware

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -5,10 +5,7 @@ import { StatusCodes } from 'http-status-codes';
 const router = Router();
 router.post('/register', userController.createUser);
 router.get('/me', (req :Request, res:Response , next:NextFunction) => {
-  res.status(200).json({
-    success: true,
-    statucCode: 200,
-    message:'your profile retrieved successfully '
-  })
+  console.log(req.cookies)
+  next()
 } ,userController.getMyprofile)
 export const userRoute = router;
