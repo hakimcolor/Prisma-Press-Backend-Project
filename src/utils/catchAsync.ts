bb8fadc79bd69bce11a9a9e3e2b854870192fe84
hakimcolor <hakimcolor777@gmail.com>
2026-08-31 22:38:01 +0600

    fix hte success statuce

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -8,7 +8,7 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
       await fn(req, res, next);
     } catch (error) {
       res.status(httpsStatus.INTERNAL_SERVER_ERROR).json({
-        success: true,
+        success: false,
         statusbar: httpsStatus.INTERNAL_SERVER_ERROR,
         message: (error as Error).message,
         data: null,
