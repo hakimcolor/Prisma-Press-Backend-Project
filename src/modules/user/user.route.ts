1bbbe0ac71fee9e333fca83c2f79370d9223777e
hakimcolor <hakimcolor777@gmail.com>
2026-09-02 23:34:09 +0600

    token

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -26,14 +26,7 @@ router.post('/register', userController.createUser);
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.cookies);
-
-    const { accessToken } = req.cookies;
-    console.log(accessToken);
-    const verifiedToken = jwtUtils.verifiedToken(
-      accessToken,
-      config.jwt_access_token_secret
-    );
+    const token =req.cookies.accessToken||req.headers.authorization
   });
 };
 
