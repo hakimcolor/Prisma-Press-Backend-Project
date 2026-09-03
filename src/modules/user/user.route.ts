0d7c66855c1f38e14b7b232c704791d75686f717
hakimcolor <hakimcolor777@gmail.com>
2026-09-03 23:27:06 +0600

    fix al

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,11 +1,8 @@
 import httpsStatus from 'http-status-codes';
 import { Request, Router, Response, NextFunction } from 'express';
 import { userController } from './user.controller';
-import { jwtUtils } from '../../utils/jwt';
-import config from '../../config';
 import { Role } from '../../../generated/prisma/enums';
-import { catchAsync } from '../../utils/catchAsync';
-import { JwtPayload } from 'jsonwebtoken';
+
 import { auth } from '../../Middlewares/auth';
 
 const router = Router();
@@ -27,8 +24,6 @@ router.get(
     console.log(accessToken);
     
 
-    
-    const requiredRoles = [Role.AUTHOR, Role.USER];
   
 
     next();
