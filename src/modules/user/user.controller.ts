3308c87944768c4c78f3ed3bbce09474c9f32c13
hakimcolor <hakimcolor777@gmail.com>
2026-09-25 22:35:08 +0600

    auth

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -39,7 +39,10 @@ const createUser = catchAsync(
     //   data: { user },
     // });
     sendResponse(res, {
-      
+      success: true,
+      StatusCodes: httpsStatus.CREATED,
+      Message: 'user registerd successfully',
+      data: { user },
     });
   }
 );
